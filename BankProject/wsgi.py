1389af8b9325498a11cd054640f33b11fce25847
Ckoskei <55988106+Ckoskei@users.noreply.github.com>
2023-03-10 20:13:26 +0000

    Creation of Bank Project

--- BankProject/wsgi.py
+++ BankProject/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BankProject.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "BankProject.settings")
 
 application = get_wsgi_application()

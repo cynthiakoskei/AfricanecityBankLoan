9c0d1f018222abb67fe130df6a44001cde58fbc2
kelvin sanga <97375935+Sanga78@users.noreply.github.com>
2023-03-13 10:09:11 +0000

    Updated register and login files

--- Banks/models.py
+++ Banks/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Bank(models.Model):
     bankName= models.CharField(max_length=100)
     image = models.ImageField()
-    interestRate = models.IntegerField()
+    interestRate = models.DecimalField(max_digits=2,decimal_places=2)
       
     def __str__(self): 
         return self.bankName

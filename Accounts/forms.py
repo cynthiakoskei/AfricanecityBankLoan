4772644334641dd21054c3fa20823114be189ba8
kelvin sanga <97375935+Sanga78@users.noreply.github.com>
2023-03-13 10:06:53 +0000

    Added save function

--- Accounts/forms.py
+++ Accounts/forms.py
@@ -4,10 +4,15 @@ from django.contrib.auth.models import User
 
 
 class CustomerSignUpForm(UserCreationForm):
-    first_name = forms.CharField(max_length=100,required=True)
-    last_name = forms.CharField(max_length=100,required=True)
     email = forms.EmailField(max_length=250,help_text='e.g youremail@gmail.com')
     
     class Meta:
         model = User
-        fields = ("first_name","last_name","username","email","password1","password2")
+        fields = ("username","email","password1","password2")
+        
+    def save(self, commit=True):
+        user = super(CustomerSignUpForm, self).save(commit=False)
+        user.email = self.cleaned_data['email']
+        if commit:
+            user.save()
+        return user

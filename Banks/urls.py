b642d8e8f5f958173f328e9ea3e1c35f45b0eba0
Kaybee-debug <87021754+Kaybee-debug@users.noreply.github.com>
2023-03-14 11:00:57 +0000

    applicant url

--- Banks/urls.py
+++ Banks/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
 	path('Views/',views.bank_list,name="bank_list"),
  	path('Create/',views.bank_create,name="bank_create"),
     path('About Us/',views.about, name="about"),
+    path('Applicant/',views.applicant_create, name="applicant"),
 ]

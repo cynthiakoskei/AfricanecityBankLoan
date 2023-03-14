11162a1957e5d721e28fbc3b6d1a7bcbb6385986
Kaybee-debug <87021754+Kaybee-debug@users.noreply.github.com>
2023-03-14 07:12:02 +0000

    register paymentinfo and bank info model

--- Banks/admin.py
+++ Banks/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from .models import Bank,Application,ContactInfo,PaymentInfo
+from .models import Bank,Application,ContactInfo,PaymentInfo,BankInfo
 # Register your models here.
 
 admin.site.register(Bank)	
 admin.site.register(Application)	
 admin.site.register(ContactInfo)
 admin.site.register(PaymentInfo)
+admin.site.register(BankInfo)
 

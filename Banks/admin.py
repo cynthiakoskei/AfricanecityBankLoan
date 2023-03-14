6b019131d9f9de631d25dfe70e3a808c32855b59
Kaybee-debug <87021754+Kaybee-debug@users.noreply.github.com>
2023-03-14 07:00:15 +0000

    register contact info model

--- Banks/admin.py
+++ Banks/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Bank,Application
+from .models import Bank,Application,ContactInfo
 # Register your models here.
 
 admin.site.register(Bank)	
 admin.site.register(Application)	
+admin.site.register(ContactInfo)
+

e1e812f7e4cef488b0b7517c1bab04f0dab63895
kelvin sanga <97375935+Sanga78@users.noreply.github.com>
2023-03-14 15:46:06 +0000

    login

--- Accounts/urls.py
+++ Accounts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('register/', views.register, name='register'),
-    path('login/',views.login_request,name='login'),
+    path('register', views.register, name='register'),
+    path('login',views.login_request,name='login'),
     path('logout/',views.logout_request,name='logout')
 ]

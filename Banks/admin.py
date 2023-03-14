ebd3d4213da431f687f345553d372e544c54eb74
Kaybee-debug <87021754+Kaybee-debug@users.noreply.github.com>
2023-03-14 06:58:19 +0000

    register admin model

--- Banks/admin.py
+++ Banks/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Bank
+from .models import Bank,Application
 # Register your models here.
 
 admin.site.register(Bank)	
+admin.site.register(Application)	

850093e7a3f17f0ffe653b1fecae1f3e6e3c5ed6
Ckoskei <55988106+Ckoskei@users.noreply.github.com>
2023-03-17 06:18:49 +0000

    Added help url

--- Banks/urls.py
+++ Banks/urls.py
@@ -2,9 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('Views/',views.bank_list,name="bank_list"),
+	path('Views',views.bank_list,name="bank_list"),
  	path('Create/',views.bank_create,name="bank_create"),
-    path('About Us/',views.about, name="about"),
+    path('About Us/',views.about, name="about us"),
     path('Applicant',views.applicant_create, name="applicant"),
     path('Features',views.features_view, name="features"),
+    path('Help/',views.help, name="help"),
 ]

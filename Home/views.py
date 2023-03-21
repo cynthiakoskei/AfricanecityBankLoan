6e36c0aa0961da9efc56e98c435348915e60be2b
Ckoskei <55988106+Ckoskei@users.noreply.github.com>
2023-03-21 18:25:14 +0000

    Added loan request view

--- Home/views.py
+++ Home/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
+from .models import Loan_Request
+from .forms import LoanRequestForm
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+
+def loan_create(request):
+    form = LoanRequestForm()
+    if request.method == "POST":
+        form = LoanRequestForm(request.POST,request.FILES)
+        if form.is_valid():
+            form.save()
+    
+    context = {
+        "form":form
+    }
+    return render(request, 'test.html', context)

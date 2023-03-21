5686189a08aefacc7ec928eba4f6982c23e336f0
Ckoskei <55988106+Ckoskei@users.noreply.github.com>
2023-03-21 12:39:18 +0000

    Added contact url

--- Banks/urls.py
+++ Banks/urls.py
@@ -2,10 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('Views',views.bank_list,name="bank_list"),
+	path('Views/',views.bank_list,name="bank_list"),
  	path('Create/',views.bank_create,name="bank_create"),
     path('About Us/',views.about, name="about us"),
-    path('Applicant',views.applicant_create, name="applicant"),
-    path('Features',views.features_view, name="features"),
+    path('Applicant/',views.applicant_create, name="applicant"),
+    path('Features/',views.features_view, name="features"),
     path('Help/',views.help, name="help"),
+    path('Contact/',views.contact, name="contact"),
 ]

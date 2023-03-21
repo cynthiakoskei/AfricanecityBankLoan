25ab2bdf7980107b0de70e0ca9ff5747a0912387
Ckoskei <55988106+Ckoskei@users.noreply.github.com>
2023-03-21 18:23:13 +0000

    Creation of Home models

--- Home/models.py
+++ Home/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.core.validators import MaxValueValidator,MinValueValidator
 
 # Create your models here.
+class Loan_Request(models.Model):
+    Loan_amount = models.FloatField(default = 10000,
+     validators= [MinValueValidator(10000),MaxValueValidator(250000
+    )])
+    # def __str__(self):
+    #   return self.Loan_amount

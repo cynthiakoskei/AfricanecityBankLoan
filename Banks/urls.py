1823e2c227e23fd9d3170b0c2764a162945c93a9
danielkwambai <57962011+danielkwambai@users.noreply.github.com>
2023-03-25 09:30:15 +0000

    modified bank urls and bank views

--- Banks/urls.py
+++ Banks/urls.py
@@ -6,7 +6,9 @@ urlpatterns = [
  	path('Create/',views.bank_create,name="bank_create"),
     path('About Us/',views.about, name="about us"),
     path('Applicant/',views.create_loan_application, name="applicant"),
-    path('Features/',views.features_view, name="features"),
+    path('Features/<int:bankName_id>/',views.feature_detail, name="features"),
     path('Help/',views.help, name="help"),
     path('Contact/',views.contact, name="contact"),
+    path('Loan Detail/<int:pk>/',views.loan_detail, name="loan_detail"),
 ]
+#<int:pk>/
--- Banks/views.py
+++ Banks/views.py
@@ -7,10 +7,10 @@ from .forms import BankForm,ApplicationForm,ContactForm
 
 # Create your views here.
 def bank_list(request):
-    listings = Bank.objects.all()
+    banks = Bank.objects.all()
     
     context = {
-        "listings":listings
+        "banks": banks
     }
    
     return render(request,"view.html",context)
@@ -41,27 +41,11 @@ def about(request):
    
     return render(request,"about.html",context)
 
-# def applicant_create(request):
-#     form = ApplicationForm()
-#     if request.method == "POST":
-#         form = ApplicationForm(request.POST,request.FILES)
-#         if form.is_valid():
-#             form.save()
-    
-#     context = {
-#         "form":form
-#     }
-   
-#     return render(request,"applicantForm.html",context)
+def feature_detail(request, bankName_id):
+    bank = Bank.objects.get(pk=bankName_id)
+    features = Features.objects.filter(bankName=bank)
+    return render(request, 'loanFeatures.html', {'bank': bank, 'features': features})
 
-def features_view(request):
-    listings = Features.objects.all()
-    
-    context = {
-        "listings":listings
-    }
-   
-    return render(request,"loanFeatures.html",context)
 
 def help(request):
     listings = Help.objects.all()
@@ -71,6 +55,36 @@ def help(request):
     }
    
     return render(request,"help.html",context)
+
+def contact(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            # Get the user's name, email, and message from the form data
+            name = form.cleaned_data['name']
+            email = form.cleaned_data['email']
+            message = form.cleaned_data['message']
+
+            # Construct the email message
+            subject = f"{name} - Loan Application Inquiry"
+            from_email = email
+            to_email = [settings.DEFAULT_FROM_EMAIL]
+            message = f"Name: {name}\nEmail: {email}\n\nMessage:\n{message}"
+
+            # Send the email
+            send_mail(subject, message, from_email, to_email, fail_silently=False)
+
+            # Show a success message to the user
+            messages.success(request, 'Your message has been sent. We will get back to you soon.')
+
+            # Redirect the user back to the contact page
+            return redirect('/Banks/Contact/')
+    else:
+        # Create an empty form
+        form = ContactForm()
+
+    return render(request, 'contact.html', {'form': form})
+
 
 
 def contact(request):
@@ -110,7 +124,7 @@ def create_loan_application(request):
             loan_application = form.save(commit=False)
             loan_application.user = request.user
             loan_application.save()
-            return redirect('loan_detail', pk=loan_application.pk)
+            return redirect('Loan Detail/', pk=loan_application.pk)
     else:
         form = ApplicationForm()
     return render(request, 'create_loan_application.html', {'form': form})

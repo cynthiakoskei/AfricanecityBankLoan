8dbb5f9094315ee97f9e6ccd170b21a457bc651d
Sanga78 <kelvinkipkosgeisanga@gmail.com>
2023-03-30 17:18:39 +0300

    models
    Added a loadexpenditure model

--- Home/models.py
+++ Home/models.py
@@ -1,13 +1,6 @@
 from django.db import models
 from django.core.validators import MaxValueValidator,MinValueValidator
 
-# Create your models here.
-# class Loan_Request(models.Model):
-#     Loan_amount = models.FloatField(default = 10000,
-#      validators= [MinValueValidator(10000),MaxValueValidator(250000
-#     )])
-#     # def __str__(self):
-#     #   return self.Loan_amount
 class Loan_Request(models.Model):
      first_name = models.CharField(max_length=10, default='John')
      last_name = models.CharField(max_length=10, default='Doe')

2751d22edc41e44ccc87b7d3a9123cd53323158c
danielkwambai <57962011+danielkwambai@users.noreply.github.com>
2023-04-15 07:48:36 +0000

    updated on the Hosts allowed

--- BankProject/settings.py
+++ BankProject/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['35.86.108.178', 'localhost']
+ALLOWED_HOSTS = ['52.26.54.232', '127.0.0.1']
 
 
 # Application definition
